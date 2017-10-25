a168a112e970e8c6b6298a86388991e0940db40e
kolorobot <rafal.borowiec@gmail.com>
2017-10-25 23:27:04 +0200

    Improving the samples

--- junit5-gradle-plugin/src/test/java/pl/codeleak/samples/junit5/gradle/SmokeTest.java
+++ junit5-gradle-plugin/src/test/java/pl/codeleak/samples/junit5/gradle/SmokeTest.java
@@ -1,13 +1,20 @@
 package pl.codeleak.samples.junit5.gradle;
 
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import pl.codeleak.samples.junit5.gradle.support.Smoke;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @Smoke
 class SmokeTest {
 
     @Test
     void smokeTest() {
-        System.out.println("Smoke test ... with system property 'smoked' = " + System.getProperty("smoked"));
+        String smoked = System.getProperty("smoked");
+
+        assumeTrue(smoked != null && !smoked.isEmpty(), "No system property found! Re-run and pass 'smoked' system property.");
+
+        System.out.println("Smoke test ... with system property 'smoked' = " + smoked);
     }
 }
