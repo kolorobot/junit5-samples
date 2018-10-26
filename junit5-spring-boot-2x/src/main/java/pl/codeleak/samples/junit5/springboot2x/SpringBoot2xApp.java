acf2871dbe200474e2e462edc0b1790e2a04beb9
rafal.borowiec <rafal.borowiec@gmail.com>
2018-10-26 18:34:28 +0200

    Kotlin, Spring 2.x samples with JUnit 5

---
+++ junit5-spring-boot-2x/src/main/java/pl/codeleak/samples/junit5/springboot2x/SpringBoot2xApp.java
@@ -0,0 +1,12 @@
+package pl.codeleak.samples.junit5.springboot2x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2xApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2xApp.class, args);
+	}
+}
---
+++ junit5-spring-boot-2x/src/test/java/pl/codeleak/samples/junit5/springboot2x/SpringBoot2xAppTests.java
@@ -0,0 +1,17 @@
+package pl.codeleak.samples.junit5.springboot2x;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBoot2xAppTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
