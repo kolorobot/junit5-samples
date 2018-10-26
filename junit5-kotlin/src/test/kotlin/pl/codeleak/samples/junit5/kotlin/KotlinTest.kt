a69bf89a594d5e6d56d1d2e02f20d67b76999bac
rafal.borowiec <rafal.borowiec@gmail.com>
2018-10-26 18:40:35 +0200

    Kotlin with JUnit 5

---
+++ junit5-kotlin/src/test/kotlin/pl/codeleak/samples/junit5/kotlin/KotlinTest.kt
@@ -0,0 +1,12 @@
+package pl.codeleak.samples.junit5.kotlin
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class `Beatifull name for my test in Kotlin` {
+
+    @Test
+    fun `Kotlin Test with JUnit 5`() {
+        Assertions.assertTrue(true)
+    }
+}
